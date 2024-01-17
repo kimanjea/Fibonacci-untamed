b6df7a76750c32e99c19a7a7aa0eceeec38828cc
Azhar Amir Kimanje <77221185+kimanjea@users.noreply.github.com>
2024-01-16 18:18:09 -0800

    Fibonacci with some edge cases

---
+++ Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Net.Mail;
+
+namespace HelloWorld
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+           int num = 7;
+           Fibonacci(num);
+           
+        }
+
+        public static void Fibonacci(int num) {
+                int sum = 0;
+                int i = 0;
+            if(num==0){
+                Console.WriteLine(num);
+            } else if (num<0) {
+                Console.WriteLine("Please enter a positive number");
+            } else {
+                while(i<num) {
+                i = i+1;
+                sum = sum + i;
+            }
+            Console.WriteLine($"The fibonacci of {num} is {sum}");
+            }
+            
+        }
+    }
+}
